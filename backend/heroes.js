e8cda6f2063d57f379f823ee0c56ddc3546d5300
Omnivers <106356087+Omnivers@users.noreply.github.com>
2022-08-10 21:16:58 +0200

    Update heroes.js

--- backend/heroes.js
+++ backend/heroes.js
@@ -1,6 +1,6 @@
 const heroes = [
     {
-				slug: "iron-man",
+	slug: "iron-man",
         name: "Iron Man",
         power: ["money"],
         color: "red",
@@ -9,7 +9,7 @@ const heroes = [
         image: "https://blog.fr.playstation.com/tachyon/sites/10/2019/07/unnamed-file-18.jpg?resize=1088,500&crop_strategy=smart"
     },
     {
-				slug: "thor",
+	slug: "thor",
         name: "Thor",
         power: ["electricty", "worthy"],
         color: "blue",
@@ -18,7 +18,7 @@ const heroes = [
         image: "https://www.bdfugue.com/media/catalog/product/cache/1/image/400x/17f82f742ffe127f42dca9de82fb58b1/9/7/9782809465761_1_75.jpg"
     },
     {
-				slug: "dardevil",
+	slug: "dardevil",
         name: "Daredevil",
         power: ["blind"],
         color: "red",
