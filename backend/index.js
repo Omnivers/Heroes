2059f4609445d972de10d951bbb6f40fc58b06e3
Omnivers <106356087+Omnivers@users.noreply.github.com>
2022-08-10 21:29:37 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
-const port = 5000
 
 const heroes = require('./routes/heroes')
-
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
-
 app.use('/heroes', heroes)
-
-
-app.listen(port, () => {
-  console.log(`Server running on ${5000}`)
+app.listen(5000, () => {
+  console.log(`Server running on 5000`)
 })
